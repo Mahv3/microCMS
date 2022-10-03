fdda1524905cf77e3df8621b4834484251816cb5
takat <takatsuka.masaya0914@gmail.com>
2022-10-04 00:33:44 +0900

    microCMS

---
+++ pages/blog/[id].js
@@ -0,0 +1,36 @@
+import { client } from "../../lib/client";
+import styles from "../../styles/Home.module.scss";
+
+export default function BlogId({ blog }) {
+  console.log(blog);
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>{blog.title}</h1>
+      <p className={styles.publishedAt}>{blog.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog.body}`,
+        }}
+        className={styles.post}
+      ></div>
+    </main>
+  );
+}
+
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "blog" });
+
+  const paths = data.contents.map((content) => `/blog/${content.id}`);
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "blog", contentId: id });
+
+  return {
+    props: {
+      blog: data,
+    },
+  };
+};
